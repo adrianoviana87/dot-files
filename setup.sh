b6ad7552a85303e6bf48069231721c71788c2536
Adriano Viana <adrianoviana87@gmail.com>
2019-03-03 16:34:20 -0300

    updated apps

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# sudo apt-get install build-essential git
+
+# sudo apt-get install wget
+# sudo apt-get install curl
+
+# checkout dotfiles
+# git clone https://github.com/adrianoviana87/dot-files.git ~/dot-files
+
+# load profile
+# echo 'source ~/dot-files/bash/profile' >> ~/.profile
+
+# load tmux
+# echo 'source ~/dotf-iles/tmux/tmux.conf' >> ~/.tmux.conf
+
+# mkdir -p ~/.local/apps
+# cd ~/.local/apps
+
+# install neovim
+# wget https://github.com/neovim/neovim/releases/download/v0.3.4/nvim.appimage
+# mv nvim.appimage nvim
+# chmod +x nvim
+
+# wget https://nodejs.org/dist/v10.15.2/node-v10.15.2-linux-x64.tar.xz
+# tar xfv node-v10.15.2-linux-x64.tar.xz
+# mv node-v10.15.2-linux-x64 node-v10
+# rm node-v10.15.2-linux-x64.tar.xz
+# mkdir ~/.npm-global
+# npm config set prefix '~/.npm-global'
+# git config --global user.name "Adriano Viana"
+# git config --global user.email "adrianoviana87@gmail.com"
+
+# cd ~
+# sudo apt-get install python python3 python-pip python3-pip ruby-dev
+# pip install neovim
+# pip3 install neovim
+# sudo gem install neovim
+# npm install -g neovim
+# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# cd ~/dot-files/vim/
+# ./install-for-neovim.sh
+
